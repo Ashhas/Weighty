f395514a8ab14b2105eaa9d5951947646a83dc5e
Aschwin B <aschwinbruyning@outlook.com>
2021-08-12 13:31:58 +0200

    + Fix keyboard overflow

--- lib/ui/add_weight/add_weight_screen.dart
+++ lib/ui/add_weight/add_weight_screen.dart
@@ -31,6 +31,7 @@ class _AddWeightScreenState extends State<AddWeightScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: _buildAppBar(),
       body: Column(
