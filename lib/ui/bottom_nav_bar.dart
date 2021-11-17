96ea05f8ebc8f339c94243e774cc7d9fde328cea
Aschwin B <aschwinbruyning@outlook.com>
2021-11-17 01:21:10 +0100

    Removed SettingsScreen from bottom nav & refactor

--- lib/ui/bottom_nav_bar.dart
+++ lib/ui/bottom_nav_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weighty/bloc/app_init/initialization_bloc.dart';
 import 'package:weighty/ui/add_weight/add_weight_screen.dart';
-import 'package:weighty/ui/settings/settings_screen.dart';
 import 'package:weighty/util/constants/ui_const.dart';
 import 'package:weighty/ui/dashboard/dashboard_screen.dart';
 import 'package:weighty/ui/history/history_screen.dart';
@@ -31,9 +30,6 @@ class _BottomNavBarState extends State<BottomNavBar> {
       case 2:
         return HistoryScreen();
         break;
-      case 3:
-        return SettingsScreen();
-        break;
       default:
         return DashboardScreen();
         break;
@@ -44,52 +40,6 @@ class _BottomNavBarState extends State<BottomNavBar> {
     setState(() {
       _selectedIndex = index;
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<InitializationBloc, InitializationState>(
-        builder: (context, state) {
-          if (state is Initialized) {
-            return Center(
-              child: _navPages(_selectedIndex),
-            );
-          } else {
-            return Container();
-          }
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard_outlined),
-            label: UiConst.dashboardTitle,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: UiConst.addTitle,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.access_time),
-            label: UiConst.historyTitle,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.more_horiz_sharp),
-            label: UiConst.settingsTitle,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedLabelStyle: TextStyle(fontSize: 12),
-        unselectedLabelStyle: TextStyle(fontSize: 12),
-        backgroundColor: _chooseBottomNavColor(),
-        selectedItemColor: _chooseSelectedItemColor(),
-        unselectedItemColor: Theme.of(context).hintColor,
-        elevation: 6.0,
-        onTap: _onItemTapped,
-      ),
-    );
   }
 
   Color _chooseBottomNavColor() {
@@ -110,5 +60,50 @@ class _BottomNavBarState extends State<BottomNavBar> {
       default:
         return Theme.of(context).focusColor;
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<InitializationBloc, InitializationState>(
+        builder: (context, state) {
+          if (state is Initialized) {
+            return Center(
+              child: _navPages(_selectedIndex),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        iconSize: 30,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            label: UiConst.dashboardTitle,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: UiConst.addTitle,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: UiConst.historyTitle,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        unselectedLabelStyle: TextStyle(fontSize: 12),
+        backgroundColor: _chooseBottomNavColor(),
+        selectedItemColor: _chooseSelectedItemColor(),
+        unselectedItemColor: Theme.of(context).hintColor,
+        elevation: 6.0,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
