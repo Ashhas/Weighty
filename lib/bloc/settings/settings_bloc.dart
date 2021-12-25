783c095f97e7e2fed4d5b45bb6b7f4b52c240c6e
Ashhas <aschwinbruyning@outlook.com>
2021-12-25 09:54:34 +0100

    bump: upgraded package_info to new version

--- lib/bloc/settings/settings_bloc.dart
+++ lib/bloc/settings/settings_bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 part 'settings_event.dart';
 
